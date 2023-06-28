b4cdd371067317a41c8739ce9f8217db94b490f6
franciscapr <francisca.paredes.pr@gmail.com>
2023-06-28 18:31:32 +0000

    Despliegue

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+require 'faker'
+
+
+10.times do
+  Character.create!(
+    name: Faker::Movies::HarryPotter.character,
+    location: Faker::Movies::HarryPotter.location,
+    house: Faker::Movies::HarryPotter.house
+  )
+end
+
